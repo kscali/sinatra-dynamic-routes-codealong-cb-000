a326cb12d41201df254bb1e428e2603acdfb7df8
Koy Saeteurn <kosatun@aol.com>
2018-09-28 02:46:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,10 +15,10 @@ class App < Sinatra::Base
 
   get "/goodbye/:name" do 
     @user_name = params[:name]
-    "Goodbye, #{user_name}."
+    "Goodbye, #{@user_name}."
   end 
   
   get '/multiply/:num1/:num2' do 
-    "#{num1}"*"#{num2}"
+    "#{@num1}"*"#{@num2}"
   end   
 end
